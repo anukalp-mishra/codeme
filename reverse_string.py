1e32cdce7c1bd3fd46c87edfdee573b11a180dea
Anukalp Mishra <58182766+im-anukalp@users.noreply.github.com>
2020-12-25 00:25:43 +0530

    Delete reverse_string.py

--- reverse_string.py
+++
@@ -1,9 +0,0 @@
-lst = []
-word = input("enter the word: ")
-for i in range(len(word)):
-    lst.append(word[i])
-rev = list(reversed(word))
-if(lst == rev):
-    print('palindrome')
-else:
-    print('not a palindrome')
